78e52e42d349548ac77817d80d71d2410b407f2a
farbod271 <farodmatin@gmail.com>
2024-04-13 19:55:22 +0200

    dd

--- root/config/settings/production.py
+++ root/config/settings/production.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["deggenhub.de"])
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+DEBUG = True
 
 # CACHES
 # ------------------------------------------------------------------------------
